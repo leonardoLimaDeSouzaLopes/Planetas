72680abb53c96e50aa0b81f589f962dcb38319bc
leonardoLimaDeSouzaLopes <91790685+leonardoLimaDeSouzaLopes@users.noreply.github.com>
2024-06-04 08:39:56 -0300

    Vou Fazer um App com os Planetas do Ben 10
    O professor passo agora para nós fazermos um App com planetas e eu escolhi fazer com os do Ben 10, quando clicar em um planeta ele abrira um modal com as informações do planeta

--- App.js
+++ App.js
@@ -1,30 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Modal, Pressable, StyleSheet, Text, View } from 'react-native';
+import { Modal, Pressable, StyleSheet, Text, View, ScrollView} from 'react-native';
 
 export default function App() {
 
   const [visivel, setVisivel] = useState(false);
 
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <StatusBar style="auto" />
       <Modal visible={visivel} >
         <View style={styles.modal}>
         <Pressable onPress={() => {setVisivel(false)}}><Text>lol</Text></Pressable>
         </View>
       </Modal>
       <Pressable onPress={() => {setVisivel(true)}}><Text>lol</Text></Pressable>
-      <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#ff0000',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   modal: {
     backgroundColor: "#0000ff",
